f35122fbaee20eb1228fa2eba30bc409aa8062c9
sajal-fareye <sajal.agrawal1@getfareye.com>
2022-10-21 15:03:37 +0530

    Day-3 Test

--- src/main/java/com/fareye/training/controller/UserController.java
+++ src/main/java/com/fareye/training/controller/UserController.java
@@ -3,9 +3,12 @@ package com.fareye.training.controller;
 import com.fareye.training.model.Todo;
 import com.fareye.training.model.User;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.HashMap;
 
 @RestController
@@ -14,35 +17,45 @@ public class UserController {
     HashMap<Integer,User>users = new HashMap<Integer,User>();
     int UserID;
 
-    @GetMapping("/users")
-    public HashMap<Integer, User> getUsers(){
+    public HashMap<Integer, User> getUserData(){
         return users;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity getUsers(){
+        return new ResponseEntity<>(users,HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable("id") int id){
-        return users.get(id);
+    public ResponseEntity getUser(@PathVariable("id") int id){
+        return new ResponseEntity<>(users.get(id),HttpStatus.OK);
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody @NotNull User user){
+    public ResponseEntity createUser(@RequestBody @NotNull User user){
         user.setUserId(UserID);
         users.put(UserID++,user);
-        return users.get(UserID-1);
+        return new ResponseEntity(user,HttpStatus.OK);
     }
 
     @PutMapping("/users/{id}")
-    public User updateUser(@PathVariable("id") int id,@RequestBody User user) throws  Exception{
+    public ResponseEntity updateUser(@PathVariable("id") int id,@RequestBody User user) throws  Exception{
         if(!users.containsKey(id)) throw new Exception("This key is not in the System");
         user.setUserId(id);
         users.put(id,user);
-        return user;
+        return new ResponseEntity(user,HttpStatus.OK);
     }
 
+//    @DeleteMapping("/users/{id}")
+//    public HashMap<Integer, User> deleteUser(@PathVariable("id") int id) throws Exception{
+//        if(!users.containsKey(id)) throw new Exception("This key is not in the System");
+//        users.remove(id);
+//        return users;
+//    }
+
     @DeleteMapping("/users/{id}")
-    public HashMap<Integer, User> deleteUser(@PathVariable("id") int id) throws Exception{
-        if(!users.containsKey(id)) throw new Exception("This key is not in the System");
+    public ResponseEntity deleteUser(@PathVariable("id") int id){
         users.remove(id);
-        return users;
+       return new ResponseEntity<>( HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/com/fareye/training/controller/UserControllerTest.java
+++ src/test/java/com/fareye/training/controller/UserControllerTest.java
@@ -2,41 +2,104 @@ package com.fareye.training.controller;
 
 import com.fareye.training.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
     RestTemplate restTemplate = new RestTemplate();
+    UserController cout = new UserController();
+
+
+
+//    @Test
+//    void postMappingUserControllerTest(){
+//
+//        User myUser = new User("sajal","Agrawal","sajal@mail");
+//        User serverUser=restTemplate.postForObject("http://localhost:8082/users", myUser, User.class);
+//        assertEquals(myUser.getEmail(),serverUser.getEmail());
+//        assertEquals(myUser.getFirstName(),serverUser.getFirstName());
+//        assertEquals(myUser.getLastName(),serverUser.getLastName());
+//    }
 
     @Test
-    void postMappingUserControllerTest(){
-
+    void postMappingUserTest(){
         User myUser = new User("sajal","Agrawal","sajal@mail");
-        User serverUser=restTemplate.postForObject("http://localhost:8082/users", myUser, User.class);
-        assertEquals(myUser.getEmail(),serverUser.getEmail());
+        ResponseEntity res = cout.createUser(myUser);
+        assertEquals(res.getStatusCode(), HttpStatus.OK);
+    }
+
+
+//    @Test
+//    void getMappingUserControllerTest(){
+//        User myUser = new User("sajal","Agrawal","sajal.agrawal38@gmail.com");
+//        User serverUser = restTemplate.getForObject("http://localhost:8082/users/0", User.class);
+//        assertEquals(myUser.getEmail(),serverUser.getEmail());
+//        assertEquals(myUser.getFirstName(),serverUser.getFirstName());
+//        assertEquals(myUser.getLastName(),serverUser.getLastName());
+//    }
+
+    @Test
+    void getMappingUserTest(){
+        int idToBeDeleted = 0;
+        ResponseEntity res = cout.getUser(idToBeDeleted);
+        assertEquals(res.getStatusCode(), HttpStatus.OK);
     }
 
     @Test
-    void getMappingUserControllerTest(){
-        User myUser = new User("sajal","Agrawal","sajal.agrawal38@gmail.com");
-        User serverUser = restTemplate.getForObject("http://localhost:8082/users/0", User.class);
-        assertEquals(myUser.getEmail(),serverUser.getEmail());
+    void getMappingAllUserTest(){
+        ResponseEntity res = cout.getUsers();
+        assertEquals(res.getStatusCode(), HttpStatus.OK);
     }
 
+
+//    @Test
+//    void putMappingUserControllerTest(){
+//        User myUser = new User("Sajal","Agrawal","sajal@mail");
+//        restTemplate.put("http://localhost:8082/users/0",myUser);
+//        User serverUser = restTemplate.getForObject("http://localhost:8082/users/0", User.class);
+//        assertEquals(myUser.getEmail(),serverUser.getEmail());
+//        assertEquals(myUser.getFirstName(),serverUser.getFirstName());
+//        assertEquals(myUser.getLastName(),serverUser.getLastName());
+//    }
+
     @Test
-    void putMappingUserControllerTest(){
+    void putMappingUserControllerTest() throws Exception {
+        int idToBeDeleted = 0;
         User myUser = new User("Sajal","Agrawal","sajal@mail");
-        restTemplate.put("http://localhost:8082/users/0",myUser);
-        User serverUser = restTemplate.getForObject("http://localhost:8082/users/0", User.class);
-        assertEquals(myUser.getEmail(),serverUser.getEmail());
+        try {
+            ResponseEntity res = cout.updateUser(idToBeDeleted,myUser);
+            assertEquals(res.getStatusCode(), HttpStatus.OK);
+        }
+        catch (Exception e){
+            assertEquals(e.getMessage(),"This key is not in the System");
+        }
     }
+
 
     @Test
     void deleteMappingUserControllerTest(){
-        restTemplate.delete("http://localhost:8082/users/0");
+//        String url = "http://localhost:8082/users/0";
+
+
+//        User myUser = new User("Sajal","Agrawal","sajal@mail");
+//        HttpEntity<User> httpEntity = new HttpEntity<>(myUser,"success");
+//        ResponseEntity<Void> responseEntity=restTemplate.exchange(url,HttpMethod.DELETE,httpEntity,void.class);
+
+
+        int idToBeDeleted = 0;
+
+        ResponseEntity res = cout.deleteUser(idToBeDeleted);
+        assertEquals(res.getStatusCode(), HttpStatus.NO_CONTENT);
+        assertEquals(cout.getUserData().containsKey(idToBeDeleted),false);
+
     }
 
 }
